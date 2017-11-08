0726283d010f9cb11be6d3ef555191cf8a201038
jet-zheng <yeyuweimeng@126.com>
2017-11-08 15:52:19 +0800

    fivenum添加校验
    fivenum添加校验

--- data_exp/codes/data_exp_fivenum.py
+++ data_exp/codes/data_exp_fivenum.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 
 
-def fivenum(df_input, df_output = 0):
+def fivenum(df_input):
     """
     功能：按列计算数据统计项（总数、均值、标准差、最小值、（25%、50%、75%）分位数、
     最大值、方差、偏度、峰度、中位数、变异系数、极差）
@@ -21,29 +21,24 @@ def fivenum(df_input, df_output = 0):
     return: DateFrame(类型为DateFrame)
     """
     
+    if True == df_input.empty:
+        return pd.DataFrame()
     
+    #df_input = pd.DataFrame(df_input)  
     
-    df_input = pd.DataFrame(df_input)
-    
-    #前三列数据
-    df_first_three_col=df_input.iloc[:, :3]
-    
-    #第四列开始为有效数据
-    df_valid_col = df_input.iloc[:, 3:]
-    
-    df_describe = df_valid_col.describe()
+    df_describe = df_input.describe()
     
     #方差
-    df_variance = df_valid_col.var().rename('var')
+    df_variance = df_input.var().rename('var')
     
     #偏度
-    df_skewness = df_valid_col.skew().rename('skew')
+    df_skewness = df_input.skew().rename('skew')
     
     #峰度
-    df_kurtosis = df_valid_col.kurt().rename('kurt')
+    df_kurtosis = df_input.kurt().rename('kurt')
     
     #中位数
-    df_median = df_valid_col.median().rename('median')
+    df_median = df_input.median().rename('median')
     
     #变异系数 = 标准差/均值
     df_std = df_describe.loc['std']    
@@ -58,7 +53,27 @@ def fivenum(df_input, df_output = 0):
     list_union = [df_variance, df_skewness, df_kurtosis, df_median, df_cov, df_range]
 
     #结果拼接
-    df_fivenum_result = df_describe.append(list_union, ignore_index=False)
-    df_output = df_fivenum_result
+    df_fivenum_result = df_describe.append(list_union, ignore_index=False)   
     
     return df_fivenum_result
+
+
+if __name__ == "__main__":
+    row1 = pd.Series(np.array([1,2,3,4]))
+    row2 = pd.Series(np.array([5,6,7,8]))
+    row3 = pd.Series(np.array([7,10,11,12]))
+    row4 = pd.Series(np.array([0,4,6,2]))
+    
+    df_test = pd.DataFrame({"A":row1, "B":row2, "C":row3, "D":row4})
+    
+    print('\n a test of fivenum() \n raw data:\n', df_test, '\n')
+    
+    print('processed data: \n', fivenum(df_test), '\n')
+    
+# =============================================================================
+#     df_null = pd.DataFrame()
+#     
+#     print('null:\n', df_null)
+# =============================================================================
+    
+    
